c2df31516280698e1a0f2af5626208b50a6ee943
PolinaTurina <polinaturina87@gmail.com>
2023-02-17 23:21:01 +0500

    сырой вывод на экран животных без ссылок и картинок

--- animals/views.py
+++ animals/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Animal
 
-# Create your views here.
+
+def animal_detail_view(request, animal_pk):
+    animal = Animal.objects.get(pk=animal_pk)
+    context = {"animal": animal}
+    return render(request, "animals/animal_detail.html", context)
+
+
+def animal_list_view(request):
+    animals = Animal.objects.all()
+    context = {"animal_list": animals}
+    return render(request, "animals/animal_list.html", context)
