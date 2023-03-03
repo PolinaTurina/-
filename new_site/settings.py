6e60b4f90756a422bb41c087be6a5b01c146432b
PolinaTurina <polinaturina87@gmail.com>
2023-03-03 17:07:33 +0500

    наследование от шаблона root+шапка сайта

--- new_site/settings.py
+++ new_site/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'new_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,6 +109,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
